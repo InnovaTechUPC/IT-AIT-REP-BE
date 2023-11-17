7f59905ee9212da17703bf656f69a6ccff9a8ee0
EcoCharlie <erikc.cortezb@gmail.com>
2023-11-17 14:37:06 -0500

    IT-AIT-REP-FE-0.0.2-SNAPSHOT: Swagger Added

---
+++ src/main/java/com/assignit/coreservice/service/base/BaseService.java
@@ -0,0 +1,30 @@
+package com.assignit.coreservice.service.base;
+
+import com.assignit.coreservice.beans.UserBean;
+import com.assignit.coreservice.model.User;
+import org.springframework.beans.BeanUtils;
+
+import java.lang.reflect.InvocationTargetException;
+
+public class BaseService<T, Bean> {
+
+    protected T toModel(Bean bean, Class<T> modelClass) {
+        T model = createInstance(modelClass);
+        BeanUtils.copyProperties(bean, model);
+        return model;
+    }
+
+    protected Bean toBean(T model, Class<Bean> beanClass) {
+        Bean bean = createInstance(beanClass);
+        BeanUtils.copyProperties(model, bean);
+        return bean;
+    }
+
+    private <U> U createInstance(Class<U> uClass) {
+        try {
+            return uClass.getDeclaredConstructor().newInstance();
+        } catch (InstantiationException | IllegalAccessException | NoSuchMethodException | InvocationTargetException e) {
+            throw new RuntimeException("Error al crear una nueva instancia", e);
+        }
+    }
+}
