f8f1dd47265c9130ba5504f282974b610fb81897
EcoCharlie <erikc.cortezb@gmail.com>
2023-11-26 14:10:42 -0500

    IT-AIT-REP-BE-0.0.2: Update YML and AiService Rest

--- src/main/java/com/assignit/coreservice/ai/restservice/AiServiceRestClient.java
+++ src/main/java/com/assignit/coreservice/ai/restservice/AiServiceRestClient.java
@@ -7,16 +7,13 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 import org.springframework.web.client.RestTemplate;
-@Component
+
 @Log4j2
+@Component
 public class AiServiceRestClient {
 
     @Autowired
     private RestTemplate restTemplate;
-
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
-    }
 
     @Value("${application.rest.client.ai-service}")
     private String url;
@@ -24,8 +21,15 @@ public class AiServiceRestClient {
     private static final String AI_SERVICE_PATH = "ai-service";
 
     public AiRestResponse aiChatService(AiRestRequest request) {
-        String url = getURL(AI_SERVICE_PATH);
-        return this.restTemplate.postForObject(url, request, AiRestResponse.class);
+        AiRestResponse aiRestResponse = new AiRestResponse();
+        try {
+            String url = getURL(AI_SERVICE_PATH);
+            aiRestResponse = this.restTemplate.postForObject(url, request, AiRestResponse.class);
+        }
+        catch (Exception e) {
+            log.error(e);
+        }
+        return aiRestResponse;
     }
 
     private String getURL(String path) {
